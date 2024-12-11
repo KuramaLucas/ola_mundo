dbc6e3cd030b039d82a0676fbf7da23f207a653e
KuramaLucas <henriquelucas615@gmail.com>
2024-12-10 22:23:07 -0300

    MaterialApp, StatefulWidget e boas praticas

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ola_mundo/app_widget.dart';
 
 main() {
-  runApp(AppWidget(title: 'fluterando4'));
-}
-
-class AppWidget extends StatelessWidget {
-  final String title;
-
-  const AppWidget({super.key, required this.title});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          title,
-          textDirection: TextDirection.ltr,
-          style: TextStyle(color: Colors.white, fontSize: 50.0),
-        ),
-      ),
-    );
-  }
+  runApp(AppWidget());
 }
